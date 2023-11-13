478162aae107d2b5af684c8cc5043e1f0cc86177
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-11-13 15:35:12 +0100

    Integration test for feature 1&2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getEvents, extractLocations } from './api';
+
 const App = ()=> {
+  const [events, setEvents] = useState([]);
+  //currentOne represents current no of events
+  const [currentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity]= useState('See all Cities')
+
+  const fetchData = async ()=>{
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === 'See all Cities' ? allEvents : allEvents.filter(event => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));  // take the 32 events
+    setAllLocations(extractLocations(allEvents));
+  }
+  // populating the list when App component is mounted
+  useEffect(()=>{
+    fetchData();
+  }, [currentCity]);
+
   return (
     <div className="App">
       <NumberOfEvents />
-      <CitySearch />
-      <EventList />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <EventList events={events}/>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,4 +1,31 @@
 import mockData from "./mock-data";
+
+const removeQuery = () => {
+  let newurl;
+  if (window.history.pushState && window.location.pathname) {
+    newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+// to get the access token
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    'https://1eyrv3ieik.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+  );
+  const { access_token } = await response.json();
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
 /**
  *
  * @param {*} events:
@@ -7,10 +34,19 @@ import mockData from "./mock-data";
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+
 export const extractLocations = (events) => {
     const extractedLocations = events.map((event) => event.location);
     const locations = [...new Set(extractedLocations)];
     return locations;
+  };
+//check token's validity
+  const checkToken = async (accessToken) => {
+    const response = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    );
+    const result = await response.json();
+    return result;
   };
   
   /**
@@ -18,5 +54,45 @@ export const extractLocations = (events) => {
    * This function will fetch the list of all events
    */
   export const getEvents = async () => {
+    if (window.location.href.startsWith('http://localhost')) {
     return mockData;
   };
+
+  // checks for a token
+  const token = await getAccessToken();
+// if access token is found
+  if (token) {
+    removeQuery();  // it is used to remove unnecessary query parameters
+    const url =  "https://1eyrv3ieik.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const response = await fetch(url);
+    const result = await response.json();
+    if (result) {
+      return result.events;
+    } else return null; 
+  }
+};
+
+   export const getAccessToken = async()=>{
+    const accessToken = localStorage.getItem('access_token');
+
+    // checks whether access token is found or not
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+// if no token is found checks for authorization code
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    //if no authorization code, redirects to Google suthorization screen
+    if (!code) {
+      const response = await fetch(
+        "https://1eyrv3ieik.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const result = await response.json();
+      const { authUrl } = result;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+   };
