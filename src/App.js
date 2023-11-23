05ddc786aece8b824a5d1fb19c9210b3bc07bd66
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-11-23 13:02:44 +0100

    Added Eventcharts & CityChart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import CityEventChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import './App.css';
 import { useEffect, useState } from 'react';
 import { getEvents, extractLocations } from './api';
@@ -36,19 +38,37 @@ const App = ()=> {
   
 
   return (
-    <div className="App">
-      <div className="alerts-container">
-        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
-        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
-        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
-      </div>
-      <CitySearch 
-      allLocations={allLocations} 
-      setCurrentCity={setCurrentCity}
-      setInfoAlert={setInfoAlert}
-       />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert ={setErrorAlert} />
-      <EventList events={events}/>
+    <div className="App " >
+       <div className='meet-app-bg'>
+       <div>
+         <h1>Meet App</h1>
+         {/* <a href='https://gitashreemahato.github.io/Meet_App/'>
+           <img src={logo} className='meet-app-logo' alt='meet-app-logo'width={200} height={80} />
+         </a> */}
+       </div>
+
+       <div className="alerts-container">
+         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+         {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+         {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+       </div>
+       
+       <CitySearch 
+       allLocations={allLocations} 
+       setCurrentCity={setCurrentCity}
+       setInfoAlert={setInfoAlert}
+        />
+       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert ={setErrorAlert} />
+      <div className='charts-container'>
+        <EventGenresChart events={events} />
+        <CityEventChart allLocations={allLocations} events={events} />
+    </div>
+    </div>
+    
+       <div >
+        <EventList events={events}/>
+       </div>
+      
     </div>
   );
 }
